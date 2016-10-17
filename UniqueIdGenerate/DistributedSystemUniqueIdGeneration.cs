3d5bd36e36ea307cf899f2e01cd6f74e500bb3b7
Alger Chen <alger.chen23@gmail.com>
2016-10-17 16:49:06 +0800

    add UniqueIdGenerate project

---
+++ UniqueIdGenerate/DistributedSystemUniqueIdGeneration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace UniqueIdGenerate
+{
+    public class DistributedSystemUniqueIdGeneration
+    {
+        private readonly ushort _machineId;
+        private long _lastTimestamp = -1L;
+        private long _sequence = 0L;
+        private static long _sequenceMask = -1L ^ -1L << 24;
+        private static readonly ushort SequenceBits = 24;
+        private static readonly ushort MachineIdBits = 8;
+        private static readonly ushort MachineIdShift = SequenceBits;
+        private static readonly ushort TimestampLeftShift = (ushort)(SequenceBits + MachineIdBits);
+
+        public DistributedSystemUniqueIdGeneration(ushort machineId)
+        {
+            _machineId = machineId;
+        }
+
+        public long NextId()
+        {
+            var timestamp = this.GetTimestamp();
+            if(_lastTimestamp == timestamp)
+            {
+                _sequence = (_sequence + 1) & _sequenceMask;
+                if(_sequence == 0)
+                {
+                    timestamp = GetNextTimestamp(_lastTimestamp);
+                }
+            }
+            else
+            {
+                _sequence = 0;
+            }
+
+            if(timestamp < _lastTimestamp)
+            {
+                try
+                {
+                    throw new Exception($"Clock moved backwards.  Refusing to generate id for {_lastTimestamp - timestamp} milliseconds");
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+            }
+
+            _lastTimestamp = timestamp;
+
+            long nextId = (timestamp << TimestampLeftShift) | (_machineId << MachineIdShift) | (_sequence);
+            Console.WriteLine("timestamp:" + timestamp +
+                ",timestampLeftShift:" + TimestampLeftShift +
+                ",nextId:" + nextId + ",machineId:" + _machineId +
+                ",sequence:" + _sequence);
+            return nextId;
+        }
+
+        private long GetNextTimestamp(long lastTimestamp)
+        {
+            var timestamp = this.GetTimestamp();
+            while(timestamp <= lastTimestamp)
+            {
+                timestamp = this.GetTimestamp();
+            }
+            return timestamp;
+        }
+
+        public long GetTimestamp()
+        {
+            return DateTime.UtcNow.ToUnixTimestamp();
+        }
+
+    }
+
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime UnixTimestampStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ToUnixTimestamp(this DateTime value)
+        {
+            //create Timespan by subtracting the value provided from
+            //the Unix Epoch
+            TimeSpan span = (value.ToUniversalTime() - UnixTimestampStart);
+
+            //return the total seconds (which is a UNIX timestamp)
+            return (long)span.TotalSeconds;
+        }
+    }
+}
---
+++ UniqueIdGenerateDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueIdGenerate;
+
+namespace UniqueIdGenerateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IdGeneration = new DistributedSystemUniqueIdGeneration(1);
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 3000; i++)
+            {
+                var id = IdGeneration.NextId();
+                Console.WriteLine("{0} : {1}",id, Convert.ToString(id,2));
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
